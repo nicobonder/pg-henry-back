63e49533fe6b39c13b2865fe83e3cec40dd20f55
fransil19 <44819334+fransil19@users.noreply.github.com>
2023-02-02 11:45:03 -0300

    Adding the ProductDetail route and controller (#14)

---
+++ src/controllers/controllerGetProductDetail.js
@@ -0,0 +1,38 @@
+const { Product, Photo, Category, Artist, Location, CategoryProduct} = require('../db.js');
+
+const getProductDetail = async(id) => {
+    return await Product.findByPk(id,
+        {
+          attributes: [
+                'id',
+                'name',
+                'Description', 
+                'StartDate',
+                'EndDate',
+                'Stock',
+                'Price',
+                'StartTime',
+                'Status'],
+          include: [
+                {
+                 model: Photo,
+                 attributes : ['Id', 'Path'] 
+                },
+                {
+                  model: Category,
+                  attributes: ['Id', 'Name'],
+                },
+                {
+                  model: Artist,
+                  attributes: ['Id', 'Name']
+                },
+                {
+                  model: Location,
+                  attributes: ['Id', 'Name', 'Address', 'Coordinates']  
+                },
+            ]
+        },
+    );
+};
+
+module.exports = { getProductDetail };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const routeGetAllProducts = require('./routeGetAllProducts');
+const routeGetProductDetail = require('./routeGetProductDetail')
 
 const router = Router();
 
 router.use('/products', routeGetAllProducts);
+router.use('/products', routeGetProductDetail);
 
 module.exports = router;
