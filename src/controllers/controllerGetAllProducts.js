712f0e3a2b31540e3d97f910ee4f30d2ea7a2499
DiegoFormoso <dformoso@yahoo.com>
2023-02-02 07:24:57 -0300

    Add get prodcuts route

---
+++ src/controllers/controllerGetAllProducts.js
@@ -0,0 +1,39 @@
+const { Product, Photo, Category, Artist, Location, CategoryProduct} = require('../db.js');
+
+const getAllProducts = async() => {
+    return await Product.findAll(
+        {
+          attributes: [
+                'id',
+                'name',
+                'Description', 
+                'StartDate',
+                'EndDate',
+                'Stock',
+                'Price',
+                'StartTime',
+                'Status'],
+          include: [
+                {
+                 model: Photo,
+                 attributes : ['Id', 'Path'] 
+                },
+                {
+                  model: Category,
+                  attributes: ['Id', 'Name'],
+                },
+                {
+                  model: Artist,
+                  attributes: ['Id', 'Name']
+                },
+                {
+                  model: Location,
+                  attributes: ['Id', 'Name', 'Address', 'Coordinates']  
+                },
+            ]
+        },
+    );
+};
+
+module.exports = { getAllProducts };
+
---
+++ src/routes/routeGetAllProducts.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const { getAllProducts } = require('../controllers/controllerGetAllProducts');
+
+const router = Router();
+
+router.get('/', async(req, res) => {
+    try {
+        const products = await getAllProducts();
+        if (products.length) return res.status(200).json(products);
+        res.status(404).json({error: `No hay productos registrados`}); 
+    }catch(e) {
+        res.status(400).json({error: e.message});
+    }
+});
+
+module.exports = router;
