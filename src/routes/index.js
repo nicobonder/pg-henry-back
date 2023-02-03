a41a996e6a4c96582ec800bd8faa7743e5596f17
soyfriss <federico.rissone@gmail.com>
2023-02-03 19:40:41 -0300

    feat(backend/admin/category): add routes for managing categories (#20)
    * feat(backend/*): add utils functions
    
    * feat(backend/admin/artist): add routes for managing artists
    
    * feat(backend/admin/category): add routes for managing categories

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const routeGetAllProducts = require('./routeGetAllProducts');
 const routeGetProductDetail = require('./routeGetProductDetail')
 const artistRouter = require('./admin/artist');
+const categoryRouter = require('./admin/category');
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use('/products', routeGetProductDetail);
 
 // Admin routes
 router.use('/admin', artistRouter);
+router.use('/admin', categoryRouter);
 
 module.exports = router;
