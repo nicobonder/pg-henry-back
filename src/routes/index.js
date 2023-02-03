4d5f3c2920e0465a23ab5568eb4103ee03b9184e
soyfriss <federico.rissone@gmail.com>
2023-02-03 19:27:02 -0300

    feat(backend/admin/artist): add admin artist (#19)
    * feat(backend/*): add utils functions
    
    * feat(backend/admin/artist): add routes for managing artists

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
 const routeGetAllProducts = require('./routeGetAllProducts');
 const routeGetProductDetail = require('./routeGetProductDetail')
+const artistRouter = require('./admin/artist');
 
 const router = Router();
 
 router.use('/products', routeGetAllProducts);
 router.use('/products', routeGetProductDetail);
+
+// Admin routes
+router.use('/admin', artistRouter);
 
 module.exports = router;
