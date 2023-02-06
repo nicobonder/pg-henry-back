e0ed832cb51ff43f21f74a3f177cbac0acd4921e
Juan Ignacio Mascarenhas <108559438+JIM90mdp@users.noreply.github.com>
2023-02-06 19:23:19 -0300

    Refactor/user-routePutUser (#31)
    * refactor(routePutUser)

--- src/routes/admin/user/routePutUser.js
+++ src/routes/admin/user/routePutUser.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 const { editUser } = require('../../../controllers/admin/user/controllerPutUser');
 
-
 const router = Router();
 
 // PUT /admin/users/{id}
@@ -10,10 +9,10 @@ router.put(
     async (req, res, next) => {
         try {
             const { id } = req.params;
-            const { userName, role, status } = req.body;
+            const { userName, role, password, status } = req.body;
             // console.log('put /user/:id req.body: ', req.body);
 
-            const user = await editUser(id, userName, role, status);
+            const user = await editUser(id, userName,password, role, status);
 
             res.status(200).json(user);
         } catch (error) {
