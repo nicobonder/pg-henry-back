17b1604bfcabbd2246255f2d517f28ffd68e9dfb
JIM90mdp <juanignaciomascarenhas@gmail.com>
2023-02-07 11:42:49 -0300

    refactor/controllers-admin-user-getUsers

--- src/controllers/admin/user/controllerGetUsers.js
+++ src/controllers/admin/user/controllerGetUsers.js
@@ -15,11 +15,12 @@ const getUsers = async (page, size, sort, filter) => {
     if (filter && Object.keys(JSON.parse(filter)).length > 0) {
         // console.log('JSON.parse(filter): ', JSON.parse(filter));
         const filterObj = JSON.parse(filter);
+        console.log("filterObj: ", filterObj)
         const idsCondition = filterObj.id ? { [Op.in]: filterObj.id } : { [Op.gt]: 0 };
-        const nameCondition = filterObj.name ? { [Op.iLike]: `${JSON.parse(filter).name}%` } : { [Op.iLike]: '%' };
+        const nameCondition = filterObj.userName ? { [Op.iLike]: `${JSON.parse(filter).userName}%` } : { [Op.iLike]: '%' };
         const statusCondition = filterObj.status ? { [Op.eq]: filterObj.status } : { [Op.in]: ['Active', 'Disabled'] };
         // console.log('idsCondition, nameCondition, statusCondition: ', idsCondition, nameCondition, statusCondition);
-        options.where = { [Op.and]: [{ id: idsCondition }, { name: nameCondition }, { status: statusCondition }] };
+        options.where = { [Op.and]: [{ id: idsCondition }, { userName: nameCondition }, { status: statusCondition }] };
     }
 
     console.log('options: ', options);
