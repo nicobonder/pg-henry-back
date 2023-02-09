69bcc4d41c47faaee13aa5e212117f574e1285a9
soyfriss <federico.rissone@gmail.com>
2023-02-09 14:26:14 -0300

    refactor(backend/product): add validations before creating/updating a product  (#43)
    * refactor(backend): add Luxon library
    
    * refactor(backend): descriptive errors in the response of express error catching endware
    
    * refactor(backend/product): add validations before creating or updating a product

--- src/app.js
+++ src/app.js
@@ -28,8 +28,8 @@ server.use('/', routes);
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
+  console.error(err.errors);
+  res.status(status).send(err.errors ? err.errors : message);
 });
 
 module.exports = server;
---
+++ src/controllers/admin/product/validateProduct.js
@@ -0,0 +1,228 @@
+const constants = require('../../../utils/constants');
+const { StatusType } = require('../../../dataType');
+const { existProduct } = require('./existProduct');
+const { getCategory } = require('../category/controllerGetCategory');
+const { DateTime } = require('luxon');
+
+async function validateProduct(data) {
+    try {
+
+        console.log('data: ', data);
+
+        // name
+        if (!data.name) {
+            return {
+                errors: {
+                    name: constants.FIELD_REQUIRED,
+                }
+            };
+        }
+        // check for products with the same name
+        if (await existProduct(data.name.trim(), data.id)) {
+            return {
+                errors: {
+                    name: constants.DUPLICATED_NAME,
+                }
+            };
+        }
+
+        // location
+        if (!data.locationId) {
+            return {
+                errors: {
+                    location: constants.FIELD_REQUIRED,
+                }
+            };
+        }
+        // artist
+        if (!data.artistId) {
+            return {
+                errors: {
+                    artist: constants.FIELD_REQUIRED,
+                }
+            };
+        }
+        // status
+        if (!StatusType.values.includes(data.status)) {
+            return {
+                errors: {
+                    status: constants.INVALID_DATA
+                }
+            };
+        }
+        // startDate (ISO 8601 format)
+        // console.log('startDate: ', data.startDate);
+        if (!data.startDate) {
+            return {
+                errors: {
+                    startDate: constants.FIELD_REQUIRED
+                }
+            };
+        }
+        if (!isValidDate(data.startDate)) {
+            return {
+                errors: {
+                    startDate: constants.INVALID_DATA
+                }
+            };
+        }
+        // endDate (ISO 8601 format)
+        if (!data.endDate) {
+            return {
+                errors: {
+                    endDate: constants.FIELD_REQUIRED
+                }
+            };
+        }
+        if (!isValidDate(data.endDate)) {
+            return {
+                errors: {
+                    endDate: constants.INVALID_DATA
+                }
+            };
+        }
+        // date range is valid ?
+        const startDate = DateTime.fromISO(data.startDate.slice(0, 10));
+        const endDate = DateTime.fromISO(data.endDate.slice(0, 10));
+        // console.log('startDate, endDate', startDate, endDate);
+        if (startDate > endDate) {
+            return {
+                errors: {
+                    startDate: constants.INVALID_RANGE,
+                    endDate: constants.INVALID_RANGE
+                }
+            };
+        }
+        // startTime
+        console.log('startTime: ', data.startTime)
+        if (!data.startTime) {
+            return {
+                errors: {
+                    startTime: constants.FIELD_REQUIRED
+                }
+            };
+        }
+        if (!isValidTime(data.startTime)) {
+            return {
+                errors: {
+                    startTime: constants.INVALID_DATA
+                }
+            };
+        }
+        // Stock
+        if (isNaN(data.stock)) {
+            return {
+                errors: {
+                    stock: constants.INVALID_DATA
+                }
+            };
+        }
+        if (data.stock < 0) {
+            return {
+                errors: {
+                    stock: constants.INVALID_STOCK
+                }
+            };
+        }
+        // Price
+        if (isNaN(data.price)) {
+            return {
+                errors: {
+                    price: constants.INVALID_DATA
+                }
+            };
+        }
+        if (data.price <= 0) {
+            return {
+                errors: {
+                    price: constants.INVALID_PRICE
+                }
+            };
+        }
+        // Categories
+        console.log('Categories: ', data.categories);
+        // if (!data.categories) {
+        //     return {
+        //         errors: {
+        //             categories: constants.FIELD_REQUIRED
+        //         }
+        //     };
+        // }
+        if (Array.isArray(data.categories)) {
+            // if (data.categories.length === 0) {
+            //     return {
+            //         errors: {
+            //             categories: constants.FIELD_REQUIRED
+            //         }
+            //     };
+            // }
+
+            for (let categoryId of data.categories) {
+                // category type
+                if (typeof categoryId !== 'number') {
+                    return {
+                        errors: {
+                            categories: constants.INVALID_ITEM_IN_LIST
+                        }
+                    };
+                }
+                // Empty category
+                if (!categoryId) {
+                    return {
+                        errors: {
+                            categories: constants.EMPTY_ITEM_IN_LIST
+                        }
+                    };
+                }
+                // Verify if temperament exists
+                const categoryInDB = await getCategory(categoryId);
+                console.log('categoryInDB: ', categoryInDB);
+
+                if (!categoryInDB) {
+                    return {
+                        errors: {
+                            categories: `${categoryId}: ${constants.ITEM_NOT_IN_LIST}`
+                        }
+                    };
+                }
+            }
+        } else {
+            if (data.categories) {
+                return `categories: ${constants.INCORRECT_TYPE}`;
+            }
+        }
+
+        return '';
+        // Enable the next 3 lines only for testing purposes
+        // return {
+        //     validationError: constants.VALIDATION_ERRORS
+        // };
+    } catch (error) {
+        console.log('Validation error: ', error);
+        return {
+            errors: {
+                errors: constants.VALIDATION_ERRORS
+            }
+        };
+    }
+}
+
+function isValidDate(value) {
+    let date = Date.parse(value);
+    if (isNaN(date)) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function isValidTime(value) {
+    // Regex to check valid
+    // time in 24-hour format
+    let regex = new RegExp(/^([01]\d|2[0-3]):?([0-5]\d):?([0-5]\d)?$/);
+
+    return value && regex.test(value);
+}
+
+module.exports = { validateProduct };
