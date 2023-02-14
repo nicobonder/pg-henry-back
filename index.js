07dc6c73f7ba2e47ad82372a32df8306be011ba2
Luciano Schmarsow <78238612+clarkos@users.noreply.github.com>
2023-02-14 00:09:23 +0000

    fix minor issues to deploy

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const server = require('./src/app.js');
 const { sequelize } = require('./src/db.js');
 const PORT = process.env.PORT || 3000;
 
-
 sequelize.authenticate().then(() => {
   console.log('Connected to the database');
   server.listen(PORT, () => {
@@ -11,16 +10,3 @@ sequelize.authenticate().then(() => {
 }).catch((error) => {
   console.error('Error connecting to the database:', error);
 });
-
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-/* const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const { setAllDb } = require('./src/controllers/client/controlletSetAllDb');
-
-// Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  setAllDb();
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
-}); */
--- src/db.js
+++ src/db.js
@@ -87,5 +87,5 @@ Review.belongsTo(Product);
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  sequelize     // para importart la conexión { conn } = require('./db.js');
 };
