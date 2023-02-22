311c776cf0820e5efb8f6a3019f6d0ba60ccf491
soyfriss <federico.rissone@gmail.com>
2023-02-22 10:26:50 -0300

    refactor(backend/*): sync models in dev env (#69)

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const server = require('./src/app.js');
 const { sequelize } = require('./src/db.js');
+const { setAllDb } = require('./src/controllers/client/controlletSetAllDb');
+require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
-sequelize.authenticate().then(() => {
-  console.log('Connected to the database');
-  server.listen(PORT, () => {
-    console.log('Server listening on port ', PORT);
+if (process.env.NODE_ENV === 'production') {
+  sequelize.authenticate().then(() => {
+    console.log('Connected to the database');
+    server.listen(PORT, () => {
+      console.log('Server listening on port ', PORT);
 
+    });
+  }).catch((error) => {
+    console.error('Error connecting to the database:', error);
   });
-}).catch((error) => {
-  console.error('Error connecting to the database:', error);
-});
+} else {
+  sequelize.sync({ force: true }).then(() => {
+    setAllDb();
+    server.listen(PORT, () => {
+      console.log(`Server listening at port ${PORT}`);
+    });
+  });
+}
