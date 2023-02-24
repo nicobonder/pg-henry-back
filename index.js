77ecc800bcea1e7a52fb41723274edefac64476c
soyfriss <federico.rissone@gmail.com>
2023-02-24 19:51:16 -0300

    fix(backend): sync db in prod (#85)

--- index.js
+++ index.js
@@ -4,21 +4,28 @@ const { setAllDb } = require('./src/controllers/client/controlletSetAllDb');
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV === 'production') {
-  sequelize.authenticate().then(() => {
-    console.log('Connected to the database');
-    server.listen(PORT, () => {
-      console.log('Server listening on port ', PORT);
+sequelize.sync({ force: true }).then(() => {
+  setAllDb();
+  server.listen(PORT, () => {
+    console.log(`Server listening at port ${PORT}`);
+  });
+});
 
-    });
-  }).catch((error) => {
-    console.error('Error connecting to the database:', error);
-  });
-} else {
-  sequelize.sync({ force: true }).then(() => {
-    setAllDb();
-    server.listen(PORT, () => {
-      console.log(`Server listening at port ${PORT}`);
-    });
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   sequelize.authenticate().then(() => {
+//     console.log('Connected to the database');
+//     server.listen(PORT, () => {
+//       console.log('Server listening on port ', PORT);
+
+//     });
+//   }).catch((error) => {
+//     console.error('Error connecting to the database:', error);
+//   });
+// } else {
+//   sequelize.sync({ force: true }).then(() => {
+//     setAllDb();
+//     server.listen(PORT, () => {
+//       console.log(`Server listening at port ${PORT}`);
+//     });
+//   });
+// }
